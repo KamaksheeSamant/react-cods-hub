5a4b38aedd45b1b8761cf23e121d2c3a0fb37918
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-09-15 10:26:22 +1000

    - Minor revert

--- src/components/code-snippet.jsx
+++ src/components/code-snippet.jsx
@@ -1,53 +1,53 @@
 import { CodeBlock } from "@atlaskit/code";
 import { Flex, xcss } from "@atlaskit/primitives";
 import Heading from "@atlaskit/heading";
-import styled from "styled-components";
+import {styled} from "styled-components";
 
-const CodeStyle = styled.div`
-  flex: 1;
-  padding: 5px;
+const HeaderContainer = styled.div`
+  width: 48%;
 `;
-
-const CodeBlockWrapper = styled.div`
-  span {
-    max-height: 150px;
-  }
-`;
-
 const headingStyles = xcss({
-  marginBlock: "space.050",
+    marginBlock: "space.050",
+});
+
+const CodeWrapper = xcss({
+    " > span": {
+        width: "48%"
+    }
 });
 
 const CodeSnippet = ({ code }) => {
   return (
-    <Flex padding="space.050" backgroundColor="color.background.discovery">
-      <CodeStyle>
-        <Heading level="h500" xcss={headingStyles}>
-          Before:
-        </Heading>
-        <CodeBlockWrapper>
-          <CodeBlock
-            language="jsx"
-            text={code.before.codeSnippet}
-            highlight={code.before.highlightedLines}
-            shouldWrapLongLines={true}
-          />
-        </CodeBlockWrapper>
-      </CodeStyle>
-      <CodeStyle>
-        <Heading level="h500" xcss={headingStyles}>
-          After:
-        </Heading>
-        <CodeBlockWrapper>
-          <CodeBlock
-            language="jsx"
-            text={code.after.codeSnippet}
-            highlight={code.after.highlightedLines}
-            shouldWrapLongLines={true}
-          />
-        </CodeBlockWrapper>
-      </CodeStyle>
-    </Flex>
+      <>
+          <Flex justifyContent="space-between" >
+
+          <HeaderContainer>
+              <Heading level="h500" xcss={headingStyles}>
+                Before:
+              </Heading>
+          </HeaderContainer>
+          <HeaderContainer>
+              <Heading level="h500" xcss={headingStyles}>
+                After:
+              </Heading>
+          </HeaderContainer>
+          </Flex>
+
+          <Flex justifyContent="space-between" xcss={CodeWrapper}>
+              <CodeBlock
+                language="jsx"
+                text={code.before.codeSnippet}
+                highlight={code.before.highlightedLines}
+                shouldWrapLongLines={true}
+              />
+              <CodeBlock
+                language="jsx"
+                text={code.after.codeSnippet}
+                highlight={code.after.highlightedLines}
+                shouldWrapLongLines={true}
+              />
+        </Flex>
+    </>
   );
 };
 
